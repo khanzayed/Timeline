e93835b471bb4eae8b4e38578c02520ff8c38894
Faraz <faraz@Farazs-MacBook-Pro.local>
2020-08-14 02:09:03 +0530

    Second commit -1

---
+++ Example/Pods/Target Support Files/Timeline/Timeline-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Timeline : NSObject
+@end
+@implementation PodsDummy_Timeline
+@end
---
+++ Timeline/Classes/SampleFile.swift
@@ -0,0 +1,13 @@
+
+
+open class SampleFile {
+    
+    public init() {
+        
+    }
+    
+    public func log(message: String) {
+        print(message)
+    }
+    
+}
